8938510410dfe5b4de7696f49c20cb3369c2d4b6
usernamecls <jun29182877@gmail.com>
2024-06-05 13:42:13 +0900

    0605 폴더 정리

--- shins_workspace/js/app.js
+++
@@ -1,9 +0,0 @@
-const contact = $('#contact');
-const myName = contact.find('#myName');
-
-myName.on('mouseover', function (){
-  $(this).addClass('active');
-})
-myName.on('mouseleave', function (){
-  $(this).removeClass('active');
-})
